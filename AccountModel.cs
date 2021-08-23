496ceca0862e85376d550584aa11b5d44a5fbf58
hpo <hpo@TEC-8200-LA0010>
2021-08-23 12:51:17 +0200

    Add project files.

---
+++ AccountModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PropertiesExample
+{
+    class AccountModel
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ CustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesExample
+{
+    class CustomerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //Creates a property of a List<AccountModel> type, which 
+        //have to be created after the CustomerModel instance have been created.
+        public List<AccountModel> AccountList { get; set; }
+        //Creates the list as the CustomerModel instance is being created
+        public List<AccountModel> accountlist = new List<AccountModel>();
+
+    }
+}
